b633c466346ae9c66ac7a440da7dd47cfe313b9e
Orje <j.brulow@kabelmail.de>
2018-10-18 20:58:14 +0200

    Start programming the Zumo with QM/QPnano

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Zumo_QM_QPnano
+This is a project to /learn/find out, how to program the Pololu Zumo with the QM/QPnano framework. It's an project for me in every way. That's the reason, why I put everything, that I think and try, into the respository.  
+I share everthing on GitHub for others to learn, get inspired or advice me.  
+Because of the fact, that I'm still a freetime beginner in the embedded world and the C language, I have to go step by step, trying to adapt the examples of the QM/QP framework for the ATmega328P to the ATmega32U4.  
+And because I'm yet not able to adapt the build and upload process within the QM/QPnano framework, I use following workaround: I program in QM & build the .ino file and then compile & load it with the Arduino IDE.  
+First I tried to adapt the blinky example. And because the framework needs a timer to generate a timetick, I tried to adapt timer/counter 4 for that. This timer is only used in the buzzer library of the Zumo. I think, I got it, eventhough the same code did not work right away. But finally it does. So, try it!  
---
+++ Zumo Programmierung.mm
@@ -0,0 +1,516 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1538675159476" ID="ID_1984811301" MODIFIED="1539524198961" TEXT="Zumo Programmierung">
+<node CREATED="1538675191977" FOLDED="true" ID="ID_187336572" MODIFIED="1539524213567" POSITION="right" TEXT="Welche Programmierumgebung benutzen?">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <ol>
+      <li>
+        Als erstes programmiere ich mit dem Micro-PlugIn in AtmelStudio, da ich dann erst einmal funktionierende Bibliotheken nutzen kann.
+      </li>
+    </ol>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1538675256437" FOLDED="true" ID="ID_1244246604" MODIFIED="1539523967482" TEXT="Arduino IDE">
+<icon BUILTIN="stop"/>
+<node CREATED="1538675410226" ID="ID_1322557574" MODIFIED="1538678791967" STYLE="fork" TEXT="F&#xfc;r">
+<node CREATED="1538677713704" ID="ID_176583447" MODIFIED="1538678787630">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      einfach, da libs von Pololu vorhanden
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1538675413979" ID="ID_1490061540" MODIFIED="1538678155132" TEXT="Wider">
+<node CREATED="1538677768994" ID="ID_1636793037" MODIFIED="1538679711948">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      keine native &quot;event driven hierachical state machine&quot;
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1538678173620" ID="ID_785810533" MODIFIED="1538678192340" TEXT="eingeschr&#xe4;nkter Komfort"/>
+</node>
+</node>
+<node CREATED="1538675285033" FOLDED="true" ID="ID_838973498" MODIFIED="1539523953414" TEXT="Atmel  oder Visual Studio">
+<icon BUILTIN="prepare"/>
+<node CREATED="1538675449424" ID="ID_1000373760" MODIFIED="1538764049050" TEXT="mit Micro">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1538675422694" ID="ID_652831508" MODIFIED="1538764044396" TEXT="F&#xfc;r">
+<node CREATED="1538677859731" ID="ID_1347018295" MODIFIED="1538677885180">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      wie Arduino IDE
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1538677876225" ID="ID_762566338" MODIFIED="1538677890101">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      komfortabler, als die pure Arduino IDE
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1538678037981" ID="ID_345883957" MODIFIED="1538678044948">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Programmiertools
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1538678072640" ID="ID_1356566462" MODIFIED="1538678078642">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Autoversvollst&#228;ndigung
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1538678097907" ID="ID_1240111497" MODIFIED="1538678103947">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Suchen und Ersetzen
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1538678131499" ID="ID_1358661549" MODIFIED="1538678131499">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Beobachtungstools
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1538675425934" ID="ID_855859390" MODIFIED="1538678232984" TEXT="Wider">
+<node CREATED="1538677768994" ID="ID_1702309130" MODIFIED="1538679727485">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      keine native &quot;event driven hierachical state machine&quot;
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1538675486948" FOLDED="true" ID="ID_1103138290" MODIFIED="1538841781229" TEXT="pur C">
+<node CREATED="1538675422694" ID="ID_649874210" MODIFIED="1538764007435" TEXT="F&#xfc;r">
+<node CREATED="1538678266895" ID="ID_1264455769" MODIFIED="1538678274322">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      sparsamerer Code
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1538678297575" ID="ID_1579550195" MODIFIED="1538678349746">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      es passt mehr&#160;in den Flash und/oder das EEPROM
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1538678369735" ID="ID_1754928512" MODIFIED="1538678377661">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      bessere Kontrolle und Fehlersuche
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1538678394233" ID="ID_1891795628" MODIFIED="1538678402681">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      h&#246;herer Lerneffekt
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1538675425934" ID="ID_671601489" MODIFIED="1538682443149" TEXT="Wider">
+<node CREATED="1538678443254" ID="ID_724000357" MODIFIED="1538678448591">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      langwierig
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1538678472062" MODIFIED="1538678472062">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      hohe Nervenbelastung
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1538678482874" MODIFIED="1538678482874">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      M&#246;glichkeit des Scheiterns
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1538675201015" FOLDED="true" ID="ID_198271477" MODIFIED="1539524189307" TEXT="QM/QPN">
+<icon BUILTIN="go"/>
+<node CREATED="1538675393608" FOLDED="true" ID="ID_1405778009" MODIFIED="1539524169366" TEXT="F&#xfc;r">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1538678508339" ID="ID_1673405776" MODIFIED="1538678508339">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      event driven und hierachical
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1538678517544" ID="ID_1622896599" MODIFIED="1538678517544">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      kombiniertes grafisches Tool mit editierbarem Code
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1538678527774" ID="ID_1142679922" MODIFIED="1538678527774">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Vertiefung meines Wissens im Umgang mit QP/QM
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1538675398387" ID="ID_229742051" MODIFIED="1539523800545" TEXT="Wider">
+<node CREATED="1539524012456" ID="ID_925637323" MODIFIED="1539524142751" TEXT="L&#xf6;sung">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <ol>
+      <li>
+        In QM designen und programmieren und .ino Datei generieren
+      </li>
+      <li>
+        mit Arduino IDE builden und uploaden
+      </li>
+    </ol>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="idea"/>
+<icon BUILTIN="ksmiletris"/>
+</node>
+<node CREATED="1538678561051" FOLDED="true" ID="ID_1252283950" MODIFIED="1539524174223">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      schwierige Adaption an den Zumo Chip
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1538678597864" ID="ID_1266363661" MODIFIED="1538678597864">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      BSP
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1538678614211" ID="ID_939651571" MODIFIED="1538678614211">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      header files
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1538678574749" ID="ID_387390444" MODIFIED="1538678574749">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      h&#246;chstwahrscheinlich m&#252;ssen alle Zumo Bibliotheken angepasst werden oder komplett eigene geschrieben
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1539274430639" FOLDED="true" ID="ID_580818972" MODIFIED="1539524178847" TEXT="&#xdc;berlegungen">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      ALT: Da neue Compiler Probleme mit meiner ehemaligen Micro Umsetzung auftraten, ist es vielleicht ratsam, meine urspr&#252;nglichen &#196;nderungen zu verwerfen und neu anzufangen.
+    </p>
+    <p>
+      Und sich dann vielleicht an Pololu zu wenden.
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      NEU: siehe und studiere Umsetzung von Nagarkar
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1539447950794" ID="ID_913780115" LINK="https://github.com/nagarkar/pololuzumo32u4" MODIFIED="1539448012023" TEXT="Nagarkar&apos;s Umsetzung"/>
+</node>
+</node>
+</node>
+<node CREATED="1538840348728" ID="ID_365015684" MODIFIED="1538840384635" POSITION="left" TEXT="Welches Designtool benutzen?"/>
+<node CREATED="1538840427200" FOLDED="true" ID="ID_1276132013" MODIFIED="1539524213567" POSITION="right" TEXT="Was soll der Zumo machen?">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <ol>
+      <li>
+        Auf Starttaste warten
+      </li>
+      <li>
+        Umfeld abtasten
+      </li>
+    </ol>
+  </body>
+</html></richcontent>
+<node CREATED="1538841643470" ID="ID_1633283069" MODIFIED="1539108622699" TEXT="Abdrehen">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <ol>
+      <li>
+        Hindernisfreiheit checken
+
+        <ol>
+          <li>
+            Hindernisfreiheit frei:
+
+            <ol>
+              <li>
+                vorw&#228;rts fahren
+              </li>
+            </ol>
+          </li>
+          <li>
+            Hindernisfreiheit nicht frei
+
+            <ol>
+              <li>
+                90&#176; nach rechts drehen
+              </li>
+            </ol>
+          </li>
+          <li>
+            nach 4 mal abdrehen, stoppen und signalisieren
+          </li>
+          <li>
+            nach 1 min von vorne anfangen
+          </li>
+        </ol>
+      </li>
+    </ol>
+  </body>
+</html></richcontent>
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1538841663613" ID="ID_1987101044" MODIFIED="1539108875128" TEXT="Umfahren"/>
+</node>
+<node CREATED="1539108920688" ID="ID_475536693" MODIFIED="1539527170655" POSITION="left" TEXT="Teilaufgaben">
+<node CREATED="1539108937358" FOLDED="true" ID="ID_137728993" MODIFIED="1539194315826" TEXT="Motoren ansteuern">
+<node CREATED="1539108974217" ID="ID_46398462" MODIFIED="1539109182480" TEXT="Gleichm&#xe4;&#xdf;igkeit synchronisieren">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Beide Encoder mitz&#228;hlen und bei Differenz den entsprechenden Motor beschleunigen, bzw. verz&#246;gern
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1539109191580" ID="ID_596105360" MODIFIED="1539109204372" TEXT="Frontsensoren abtasten"/>
+<node CREATED="1539109206683" ID="ID_396289365" MODIFIED="1539109217958" TEXT="Seitensensoren abtasten"/>
+<node CREATED="1539109250003" ID="ID_335363733" MODIFIED="1539109265385" TEXT="Start/Stopp Taste"/>
+<node CREATED="1539109306984" ID="ID_1990753561" MODIFIED="1539109518683" TEXT="Status LED">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      gr&#252;n = okay
+    </p>
+    <p>
+      gelb = suche
+    </p>
+    <p>
+      rot = gebe auf
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1539527183106" ID="ID_1323765566" MODIFIED="1539530933210" TEXT="timetick generieren">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Timer 4 benutzen (wird sonst vom Buzzer benutzt)
+    </p>
+    <p>
+      Miro benutzt timer 2 im CTC mode. Ich glaube ich muss den normal mode nehmen und TOV. Da schreibt nagarkar allerdings, dass der overflow schon anderweitig benutzt wird. Also weiterlesen.!?
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1539194358878" FOLDED="true" ID="ID_188788871" MODIFIED="1539524213568" POSITION="right" TEXT="Fragen">
+<node CREATED="1539194370910" ID="ID_521364719" MODIFIED="1539194380004" TEXT="Timer 3 benutzen?"/>
+</node>
+<node CREATED="1539195597155" FOLDED="true" ID="ID_1394309608" MODIFIED="1539524213568" POSITION="right" TEXT="Infos">
+<node CREATED="1539195605739" ID="ID_819545712" MODIFIED="1539196565475" TEXT="32U4 = Micro (= Leonardo)"/>
+<node CREATED="1539196101019" ID="ID_910783163" MODIFIED="1539196185602" TEXT="Motor = 410 RPM = 65 cm/s"/>
+<node CREATED="1539197559942" ID="ID_310958718" MODIFIED="1539199999929" TEXT="Encoderimpulse/Umdrehung = 75.81 &#xd7; 12 &#x2248; 909.7 CPR">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      To reduce the required number of interrupt pins, the Zumo 32U4 main board XORs together both channels of each encoder and connects the resulting signal to an interrupt pin, while channel B of each encoder is connected to a non-interrupt pin:
+    </p>
+    <p>
+      &#8226; Digital pin 7, or PE6, reads the right encoder XORed signal using external interrupt INT6.
+    </p>
+    <p>
+      &#8226; Digital pin 8, or PB4, reads the left encoder XORed signal using pin change interrupt PCINT4.
+    </p>
+    <p>
+      &#8226; Digital pin 23 (analog pin 5), or PF0, reads the right encoder channel B.
+    </p>
+    <p>
+      &#8226; Pin PE2 reads the left encoder channel B.
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+</map>
