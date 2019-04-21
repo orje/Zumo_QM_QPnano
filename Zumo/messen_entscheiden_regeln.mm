5f5c73e4baa559ffe9881178fc4727ec850f2ce0
orje <j.brulow@kabelmail.de>
2019-04-21 21:45:46 +0200

    advancing

--- Zumo/messen_entscheiden_regeln.mm
+++ Zumo/messen_entscheiden_regeln.mm
@@ -2,7 +2,7 @@
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
 <node CREATED="1555441858478" ID="ID_1136660202" MODIFIED="1555441885403" TEXT="messen, entscheiden, regeln">
 <node CREATED="1555443143286" ID="ID_1820265369" MODIFIED="1555443149532" POSITION="right" TEXT="Abstand">
-<node CREATED="1555442069002" ID="ID_330990518" MODIFIED="1555780664612" TEXT="asymetrisch">
+<node CREATED="1555442069002" ID="ID_330990518" MODIFIED="1555872047822" TEXT="asymetrisch">
 <richcontent TYPE="NOTE"><html>
   <head>
     
@@ -14,8 +14,21 @@
   </body>
 </html>
 </richcontent>
-<node CREATED="1555442511432" ID="ID_1868867466" MODIFIED="1555679195626" STYLE="fork" TEXT="s = 1 - 5">
-<node CREATED="1555442111461" ID="ID_1177655817" MODIFIED="1555681120548" TEXT="&#x2206;2">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1555442511432" ID="ID_1868867466" MODIFIED="1555873901663" STYLE="fork" TEXT="s &lt;= 5 &amp; &#x2206; != 0">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font size="4">? abs(leftBarrier - rightBarrier) != 0 </font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="button_ok"/>
+<node CREATED="1555442111461" ID="ID_1177655817" MODIFIED="1555871134622" TEXT="&#x2206;2">
 <richcontent TYPE="NOTE"><html>
   <head>
     
@@ -41,14 +54,20 @@
     </p>
   </body>
 </html></richcontent>
-<node CREATED="1555442280253" ID="ID_608521" MODIFIED="1555678723736" TEXT="ignorieren"/>
+<icon BUILTIN="button_cancel"/>
+<node CREATED="1555442280253" ID="ID_608521" MODIFIED="1555872490726" TEXT="ignorieren">
+<icon BUILTIN="button_cancel"/>
 </node>
-<node CREATED="1555442095366" ID="ID_1075527233" MODIFIED="1555681129185" TEXT="&#x2206;1">
+</node>
+<node CREATED="1555442095366" ID="ID_1075527233" MODIFIED="1555873784386" TEXT="&#x2206;1">
 <richcontent TYPE="NOTE"><html>
   <head>
     
   </head>
   <body>
+    <p>
+      <font size="4">0 : 1 </font>
+    </p>
     <p>
       <font size="4">1 : 2 </font>
     </p>
@@ -68,7 +87,9 @@
       <font size="4">? abs(leftBarrier - rightBarrier) == 1</font>
     </p>
   </body>
-</html></richcontent>
+</html>
+</richcontent>
+<icon BUILTIN="button_cancel"/>
 <node CREATED="1555442268867" ID="ID_1058036249" MODIFIED="1555681741209" TEXT="ausweichen">
 <richcontent TYPE="NOTE"><html>
   <head>
@@ -113,8 +134,10 @@
 </node>
 </node>
 <node CREATED="1555442041869" ID="ID_1639016630" MODIFIED="1555693379567" TEXT="symetrisch">
-<node CREATED="1555442135814" ID="ID_477876947" MODIFIED="1555680859827" TEXT="s = 0 &amp; v &lt;= e">
-<node CREATED="1555442234138" ID="ID_1757857494" MODIFIED="1555443793388" TEXT="beschleunigen">
+<node CREATED="1555442135814" ID="ID_477876947" MODIFIED="1555874483273" TEXT="s = 0">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1555874415402" ID="ID_791914318" MODIFIED="1555874422209" TEXT=" v &lt;= e">
+<node CREATED="1555442234138" ID="ID_1757857494" MODIFIED="1555874424715" TEXT="beschleunigen">
 <richcontent TYPE="NOTE"><html>
   <head>
     
@@ -127,7 +150,32 @@
 </html></richcontent>
 </node>
 </node>
-<node CREATED="1555443774071" ID="ID_1608642089" MODIFIED="1555678695005" STYLE="fork" TEXT="s = 1 - 5">
+</node>
+<node CREATED="1555443774071" ID="ID_1608642089" MODIFIED="1555875385942" STYLE="fork" TEXT="s = 1 - 5">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font size="4">1 : 1 </font>
+    </p>
+    <p>
+      <font size="4">2 : 2 </font>
+    </p>
+    <p>
+      <font size="4">3 : 3 </font>
+    </p>
+    <p>
+      <font size="4">4 : 4 </font>
+    </p>
+    <p>
+      <font size="4">5 : 5 </font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="button_ok"/>
 <node CREATED="1555442244772" ID="ID_1232303823" MODIFIED="1555680979479" TEXT="regeln">
 <richcontent TYPE="NOTE"><html>
   <head>
@@ -141,7 +189,8 @@
 </html></richcontent>
 </node>
 </node>
-<node CREATED="1555442146122" ID="ID_285132072" MODIFIED="1555678706104" TEXT="s = 6">
+<node CREATED="1555442146122" ID="ID_285132072" MODIFIED="1555875702817" TEXT="s = 6">
+<icon BUILTIN="button_ok"/>
 <node CREATED="1555442258170" ID="ID_1443366646" MODIFIED="1555682161288" TEXT="wenden">
 <richcontent TYPE="NOTE"><html>
   <head>
