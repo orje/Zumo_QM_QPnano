062850383a376ca6b5d11d3b361de7e20dee77a1
Orje <j.brulow@kabelmail.de>
2019-05-04 21:46:27 +0200

    work on

--- Zumo/messen_entscheiden_Parameter_regeln.mm
+++ Zumo/messen_entscheiden_Parameter_regeln.mm
@@ -48,213 +48,7 @@
   </body>
 </html></richcontent>
 <node CREATED="1555443143286" ID="ID_1820265369" MODIFIED="1555443149532" POSITION="right" TEXT="Abstand">
-<node CREATED="1555442069002" ID="ID_330990518" MODIFIED="1555872047822" TEXT="asymetrisch">
-<richcontent TYPE="NOTE"><html>
-  <head>
-    
-  </head>
-  <body>
-    <p>
-      <font size="4">? leftBarrier &lt;&gt; rightBarrier</font>
-    </p>
-  </body>
-</html></richcontent>
-<icon BUILTIN="button_ok"/>
-<node CREATED="1555442511432" ID="ID_1868867466" MODIFIED="1556897316967" STYLE="fork" TEXT="s &lt;= 5">
-<icon BUILTIN="button_ok"/>
-<node CREATED="1555442111461" ID="ID_1177655817" MODIFIED="1555871134622" TEXT="&#x2206;2">
-<richcontent TYPE="NOTE"><html>
-  <head>
-    
-  </head>
-  <body>
-    <p>
-      <font size="4">1 : 3 </font>
-    </p>
-    <p>
-      <font size="4">2 : 4 </font>
-    </p>
-    <p>
-      <font size="4">3 : 5 </font>
-    </p>
-    <p>
-      <font size="4">4 : 6 </font>
-    </p>
-    <p>
-      <font size="4">&#160;</font>
-    </p>
-    <p>
-      <font size="4">? abs(leftBarrier - rightBarrier) &gt; 1</font>
-    </p>
-  </body>
-</html></richcontent>
-<icon BUILTIN="button_cancel"/>
-<node CREATED="1555442280253" ID="ID_608521" MODIFIED="1555872490726" TEXT="ignorieren">
-<icon BUILTIN="button_cancel"/>
-</node>
-</node>
-<node CREATED="1555442095366" ID="ID_1075527233" MODIFIED="1556462081958" TEXT="&#x2206;1">
-<richcontent TYPE="NOTE"><html>
-  <head>
-    
-  </head>
-  <body>
-    <p>
-      <font size="4">0 : 1 </font>
-    </p>
-    <p>
-      <font size="4">1 : 2 </font>
-    </p>
-    <p>
-      <font size="4">2 : 3 </font>
-    </p>
-    <p>
-      <font size="4">3 : 4 </font>
-    </p>
-    <p>
-      <font size="4">4 : 5 </font>
-    </p>
-    <p>
-      <font size="4">&#160;</font>
-    </p>
-    <p>
-      <font size="4">? abs(leftBarrier - rightBarrier) == 1</font>
-    </p>
-  </body>
-</html></richcontent>
-<icon BUILTIN="button_cancel"/>
-</node>
-<node CREATED="1556461993232" ID="ID_316459498" MODIFIED="1556462152611" TEXT="l &gt; r">
-<richcontent TYPE="NOTE"><html>
-  <head>
-    
-  </head>
-  <body>
-    <p>
-      <font size="4">? leftBarrier &gt; rightBarrier</font>
-    </p>
-  </body>
-</html></richcontent>
-<node CREATED="1555442268867" ID="ID_1058036249" MODIFIED="1556597562064" TEXT="ausweichen">
-<richcontent TYPE="NOTE"><html>
-  <head>
-    
-  </head>
-  <body>
-    <!--
-        body { font-family: Cambria; font-size: 28pt }
-        p { margin-top: 0 }
-      -->
-    
-
-    <p>
-      <font size="4">leftSpeed = aR * (rightBarrier - 1) + e; </font>
-    </p>
-    <p style="margin-top: 0px; margin-bottom: 0px; margin-left: 0px; margin-right: 0px; text-indent: 0px">
-      <font size="4">rightSpeed = aA * (leftBarrier - 1) + e;</font>
-    </p>
-  </body>
-</html></richcontent>
-<icon BUILTIN="button_ok"/>
-<icon BUILTIN="attach"/>
-<icon BUILTIN="attach"/>
-</node>
-</node>
-<node CREATED="1556462012102" ID="ID_157533692" MODIFIED="1556462164314" TEXT="r &gt; l">
-<richcontent TYPE="NOTE"><html>
-  <head>
-    
-  </head>
-  <body>
-    <p>
-      <font size="4">else</font>
-    </p>
-  </body>
-</html></richcontent>
-<node CREATED="1555442268867" ID="ID_1220289525" MODIFIED="1556597565754" TEXT="ausweichen">
-<richcontent TYPE="NOTE"><html>
-  <head>
-    
-  </head>
-  <body>
-    <!--
-        body { font-family: Cambria; font-size: 28pt }
-        p { margin-top: 0 }
-      -->
-    
-
-    <p>
-      <font size="4">leftSpeed = aA * (rightBarrier - 1) + e;</font>
-    </p>
-    <p style="margin-top: 0px; margin-bottom: 0px; margin-left: 0px; margin-right: 0px; text-indent: 0px">
-      <font size="4">rightSpeed = aR * (leftBarrier - 1) + e;</font>
-    </p>
-    <p style="margin-top: 0px; margin-bottom: 0px; margin-left: 0px; margin-right: 0px; text-indent: 0px">
-      
-    </p>
-  </body>
-</html></richcontent>
-<icon BUILTIN="button_ok"/>
-<icon BUILTIN="attach"/>
-<icon BUILTIN="attach"/>
-</node>
-</node>
-</node>
-<node CREATED="1555442146122" ID="ID_285132072" MODIFIED="1556462264740" TEXT="s = 6">
-<richcontent TYPE="NOTE"><html>
-  <head>
-    
-  </head>
-  <body>
-    <p>
-      else
-    </p>
-  </body>
-</html></richcontent>
-<icon BUILTIN="button_ok"/>
-<node CREATED="1555442258170" ID="ID_1443366646" MODIFIED="1556596719517" TEXT="wenden">
-<richcontent TYPE="NOTE"><html>
-  <head>
-    
-  </head>
-  <body>
-    <p>
-      <font size="4">? leftBarrier == 6 { </font>
-    </p>
-    <p>
-      <font size="4">leftSpeed = turnSpeed; </font>
-    </p>
-    <p>
-      <font size="4">rightSpeed = 0; </font>
-    </p>
-    <p style="margin-top: 0px; margin-bottom: 0px; margin-left: 0px; margin-right: 0px; text-indent: 0px">
-      <font size="4">} </font>
-    </p>
-    <p style="margin-top: 0px; margin-bottom: 0px; margin-left: 0px; margin-right: 0px; text-indent: 0px">
-      <font size="4">&#160;</font>
-    </p>
-    <p style="margin-top: 0px; margin-bottom: 0px; margin-left: 0px; margin-right: 0px; text-indent: 0px">
-      <font size="4">else { </font>
-    </p>
-    <font size="4">leftSpeed = 0; </font>
-
-    <p style="margin-top: 0px; margin-bottom: 0px; margin-left: 0px; margin-right: 0px; text-indent: 0px">
-      <font size="4">rightSpeed = turnSpeed; </font>
-    </p>
-    <p style="margin-top: 0px; margin-bottom: 0px; margin-left: 0px; margin-right: 0px; text-indent: 0px">
-      <font size="4">}&#160; </font>
-    </p>
-    <p>
-      <font size="4">&#160;</font>
-    </p>
-  </body>
-</html></richcontent>
-<icon BUILTIN="button_ok"/>
-<icon BUILTIN="attach"/>
-</node>
-</node>
-</node>
-<node CREATED="1555442041869" ID="ID_1639016630" MODIFIED="1555693379567" TEXT="symetrisch">
+<node CREATED="1555442041869" ID="ID_1639016630" MODIFIED="1556996122876" TEXT="symetrisch">
 <node CREATED="1555442135814" ID="ID_477876947" MODIFIED="1555874483273" TEXT="s = 0">
 <icon BUILTIN="button_ok"/>
 <node CREATED="1555874415402" ID="ID_791914318" MODIFIED="1556295011474" TEXT=" v &lt; e">
@@ -311,6 +105,154 @@
 </html></richcontent>
 <icon BUILTIN="button_ok"/>
 <icon BUILTIN="attach"/>
+<icon BUILTIN="attach"/>
+</node>
+</node>
+</node>
+<node CREATED="1555442069002" ID="ID_330990518" MODIFIED="1556996122885" TEXT="asymetrisch">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font size="4">? leftBarrier &lt;&gt; rightBarrier</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="button_ok"/>
+<node CREATED="1555442511432" ID="ID_1868867466" MODIFIED="1556897316967" STYLE="fork" TEXT="s &lt;= 5">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1556461993232" ID="ID_316459498" MODIFIED="1556996139512" TEXT="l &gt; r">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font size="4">? leftBarrier &gt; rightBarrier</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1555442268867" ID="ID_1058036249" MODIFIED="1556597562064" TEXT="ausweichen">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <!--
+        body { font-family: Cambria; font-size: 28pt }
+        p { margin-top: 0 }
+      -->
+    
+
+    <p>
+      <font size="4">leftSpeed = aR * (rightBarrier - 1) + e; </font>
+    </p>
+    <p style="margin-top: 0px; margin-bottom: 0px; margin-left: 0px; margin-right: 0px; text-indent: 0px">
+      <font size="4">rightSpeed = aA * (leftBarrier - 1) + e;</font>
+    </p>
+  </body>
+</html></richcontent>
+<icon BUILTIN="button_ok"/>
+<icon BUILTIN="attach"/>
+<icon BUILTIN="attach"/>
+</node>
+</node>
+<node CREATED="1556462012102" ID="ID_157533692" MODIFIED="1556996140041" TEXT="r &gt; l">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font size="4">else</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1555442268867" ID="ID_1220289525" MODIFIED="1556597565754" TEXT="ausweichen">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <!--
+        body { font-family: Cambria; font-size: 28pt }
+        p { margin-top: 0 }
+      -->
+    
+
+    <p>
+      <font size="4">leftSpeed = aA * (rightBarrier - 1) + e;</font>
+    </p>
+    <p style="margin-top: 0px; margin-bottom: 0px; margin-left: 0px; margin-right: 0px; text-indent: 0px">
+      <font size="4">rightSpeed = aR * (leftBarrier - 1) + e;</font>
+    </p>
+    <p style="margin-top: 0px; margin-bottom: 0px; margin-left: 0px; margin-right: 0px; text-indent: 0px">
+      
+    </p>
+  </body>
+</html></richcontent>
+<icon BUILTIN="button_ok"/>
+<icon BUILTIN="attach"/>
+<icon BUILTIN="attach"/>
+</node>
+</node>
+</node>
+<node CREATED="1555442146122" ID="ID_285132072" MODIFIED="1556462264740" TEXT="s = 6">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      else
+    </p>
+  </body>
+</html></richcontent>
+<icon BUILTIN="button_ok"/>
+<node CREATED="1555442258170" ID="ID_1443366646" MODIFIED="1556991395665" TEXT="wenden">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font size="4">? leftBarrier == 6 { </font>
+    </p>
+    <p>
+      <font size="4">leftSpeed = turnSpeed; </font>
+    </p>
+    <p>
+      <font size="4">rightSpeed = 0; </font>
+    </p>
+    <p style="margin-top: 0px; margin-bottom: 0px; margin-left: 0px; margin-right: 0px; text-indent: 0px">
+      <font size="4">} </font>
+    </p>
+    <p style="margin-top: 0px; margin-bottom: 0px; margin-left: 0px; margin-right: 0px; text-indent: 0px">
+      <font size="4">&#160;</font>
+    </p>
+    <p style="margin-top: 0px; margin-bottom: 0px; margin-left: 0px; margin-right: 0px; text-indent: 0px">
+      <font size="4">else { </font>
+    </p>
+    <font size="4">leftSpeed = 0; </font>
+
+    <p style="margin-top: 0px; margin-bottom: 0px; margin-left: 0px; margin-right: 0px; text-indent: 0px">
+      <font size="4">rightSpeed = turnSpeed; </font>
+    </p>
+    <p style="margin-top: 0px; margin-bottom: 0px; margin-left: 0px; margin-right: 0px; text-indent: 0px">
+      <font size="4">}&#160; </font>
+    </p>
+    <p>
+      <font size="4">&#160;</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="button_ok"/>
 <icon BUILTIN="attach"/>
 </node>
 </node>
